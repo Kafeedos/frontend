53a933cf4c8173b19a9451e895a192692d0badd8
Abdulkafid <kafeedos@gmail.com>
2024-12-13 15:24:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+import './Components/Navbar/Navbar.css';
+function App() {
+  return (
+    <div >
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
