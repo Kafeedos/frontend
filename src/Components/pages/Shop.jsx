5e15bad018b756b18cc7df2df8a582592de55566
Abdulkafid <kafeedos@gmail.com>
2024-12-23 12:45:45 +0100

    added Offers page

--- src/Components/pages/Shop.jsx
+++ src/Components/pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Hero } from '../Hero/Hero'
 import { Popular } from '../Popular/Popular'
+import { Offers } from '../Offers/Offers'
 
 export const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offers/>
     </div>
   )
 }
